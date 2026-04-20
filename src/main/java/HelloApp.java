12b591e5e52b85a3d3d81631c553d15bbb811227
sheryljohn1000-boop <sheryljohn1000@gmail.com>
2026-04-20 09:07:19 +0530

    Coded UC7: Display Hello with multiple args using String.join() method

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,18 +1,12 @@
 public class HelloApp{
     public static void main(String[] args) {
-           if (args.length == 0) {
-            System.out.println("Hello, World!");
-        } else {
-            // Build greeting string with trailing delimiter
-            StringBuilder nameBuilder = new StringBuilder();
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
+           String names = "World";
 
-            // Remove the last ", " using substring
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-
-            System.out.println("Hello, " + names + "!");
+        // If arguments are provided, join them with commas
+        if (args.length > 0) {
+            names = String.join(", ", args);
         }
+
+        System.out.println("Hello, " + names + "!");
     }
 }
